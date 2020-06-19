2b4d740cc4e3d6a7f915dc12f76194e650349c01
Jeff King <jazfunk@gmail.com>
2020-06-18 23:16:48 -0400

    Design header, main, and footer  layout, add Darth Vader background

--- src/AppFooter.js
+++ src/AppFooter.js
@@ -1,14 +1,22 @@
-import React, {Component} from 'react'
-import jklogo from "./images/titleGraphicWithLogoOPTIMIZED.svg"
+import React, { Component } from "react";
+import jklogo from "./images/titleGraphicWithLogoOPTIMIZED.svg";
+import "./App.css";
 
 class AppFooter extends Component {
   render() {
     return (
       <footer className="App-footer">
-        <img src={jklogo} alt="Jeff King" />
+        <img id="jk-logo" src={jklogo} alt="Jeff King" />
+        <div>
+          <a href="https://github.com/jazfunk">GitHub</a>
+          &nbsp;|&nbsp;
+          <a href="https://www.linkedin.com/in/jeffking222/">LinkedIn</a>
+          &nbsp;|&nbsp;
+          <a href="https://www.jeff-king.net/">Website</a>
+        </div>
       </footer>
-    )
+    );
   }
 }
 
-export default AppFooter
+export default AppFooter;
--- src/AppHeader.js
+++ src/AppHeader.js
@@ -5,16 +5,10 @@ import "./App.css";
 class AppHeader extends Component {
   render() {
     return (
-      <header className="App-header">
-        <div className="container">
-          <div className="row">
-            <div className="col-sm-2">
-              <img src={logo} className="App-logo" alt="logo" />
-            </div>
-            <div className="col-sm-10">
-              <h4>STAR WARS API</h4>
-            </div>
-          </div>
+      <header>
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          STAR WARS API
         </div>
       </header>
     );
